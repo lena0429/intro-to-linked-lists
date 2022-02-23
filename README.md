67933f08b99c14ce7eb081587b8814d1b1356d7c
Lena Hou <lenahou2013@gmail.com>
2022-02-23 11:05:59 -0500

    defined recursive traversal function

--- README.md
+++ README.md
@@ -12,5 +12,26 @@ A linked list contains a bunch of nodes linked together. We can consider a linke
 * An array must be stored contiguously (right next to each other) in memory. 
 * An linked list does not require its nodes stored contiguously in memory. 
 
+## Traverse A Linked List
+If I want to traverse through a linked list and print out every node, I would need to continually update a current pointer. In the context of my function, the only important variable I am going to track is `current`, which starts at the head of my linked list - a node.  
+
+* Iterative Function
+const printLinkedList = (head) => {
+   let current = head;
+   while (current !== null) {
+       console.log(current.val);
+       current = current.next;
+   }
+}
+
+* Recursive Function
+const printRecursive = (head) => {
+    let current = head
+    if (current === null) return;    // base case
+
+    console.log(current.val);
+    printRecursive(current.next);
+}
+
 ## Usage
 $ node linkedLists.js
--- linkedLists.js
+++ linkedLists.js
@@ -17,16 +17,33 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-const printLinkedList = (head) => {
+//const printLinkedList = (head) => {
 // if I want to traverse through a linked list and print out every node,
 // I would need to continually update a current pointer. 
-let current = head;
+// In this context, the only important variable I am going to track is `current`, which starts at the head of my linked list - a node.  
+// let current = head;
 
-  while (current !== null) {
-      console.log(current.val);
-      current = current.next;
-  }
-}; 
+// while the current node is not equal to null, iterate through the list and print
+// while the current node is equal to null, we reach the statement nul !== null
+// which return false, and make us exit the while loop
+//  while (current !== null) {
+//      console.log(current.val);
+//      current = current.next;
+//  }
+//}; 
+
+// recursive function
+const recursivePrint = (head) => {
+    let current = head
+    // base case
+    if(current === null) {
+        return;
+    }
+    console.log(current.val)
+    recursivePrint(current.next)
+}
 
 // excute this traverse function
-printLinkedList(a);
+// printLinkedList(a);
+
+recursivePrint(a)
