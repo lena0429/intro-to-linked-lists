429e9e4d8b166f7645b0e2c6cbe6ac4a5d0e3239
Lena Hou <lenahou2013@gmail.com>
2022-02-23 14:43:42 -0500

    defined iterative solution to traverse

---
+++ iterativeSolutionToTraverse.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+function iterativeSolution(head){
+    let current = head;
+
+    while(current !== null){
+        console.log(current.val)
+        current = current.next
+    }
+};
+
+iterativeSolution(b);
---
+++ recursiveSolutionToTraverse.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+function recursiveSolution(head){
+    let current = head;
+    if (current === null) return;
+
+    console.log(current.val);
+    recursiveSolution(current.next);
+}
+
+recursiveSolution(a)
+
